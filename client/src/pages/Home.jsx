29f1658332688c1cba2988f9ca9f5d59edf3727d
Alejandro Campos <acamposcar@gmail.com>
2022-07-25 22:46:09 +0200

    Center home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,18 +4,21 @@ import piecesImg from '../assets/pieces.png'
 import CreateGame from '../components/CreateGame'
 const Home = () => {
   return (
-    <Grid gridTemplateColumns={{ base: '1fr', lg: '1fr 1fr' }} justifyItems={{ base: 'center', lg: 'stretch' }} columnGap='130px' rowGap={12}>
-      <Box bgColor='bgDark' width='100%' paddingBottom={4}>
-        <Heading textAlign={{ base: 'center', lg: 'right' }} fontSize={{ base: 80, lg: 90 }} fontWeight={300}>Let's Play</Heading>
-        <Heading textAlign={{ base: 'center', lg: 'right' }} fontSize={{ base: 80, lg: 90 }}>Chess</Heading>
-      </Box>
-      <CreateGame />
-      <Hide below='lg'>
-        <Center gridColumn='span 2'>
-          <Image height='300px' src={piecesImg} alt='Chess Pieces' />
-        </Center>
-      </Hide>
-    </Grid>
+    <Center h={{ base: '', lg: '90vh' }}>
+      <Grid gridTemplateColumns={{ base: '1fr', lg: '1fr 1fr' }} justifyItems={{ base: 'center', lg: 'stretch' }} columnGap='130px' rowGap={12} w='100%' >
+        <Box bgColor='bgDark' width='100%' paddingBottom={4}>
+          <Heading textAlign={{ base: 'center', lg: 'right' }} fontSize={{ base: 80, lg: 90 }} fontWeight={300}>Let's Play</Heading>
+          <Heading textAlign={{ base: 'center', lg: 'right' }} fontSize={{ base: 80, lg: 90 }}>Chess</Heading>
+        </Box>
+        <CreateGame />
+        <Hide below='lg'>
+          <Center gridColumn='span 2'>
+            <Image height='300px' src={piecesImg} alt='Chess Pieces' />
+          </Center>
+        </Hide>
+      </Grid>
+    </Center>
+
   )
 }
 
